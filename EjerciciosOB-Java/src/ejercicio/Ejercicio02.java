807340b49d6e5383f4f9e4e41c72b43dc2e42db6
Admin <Admin@DESKTOP-FNNFMRC.home>
2023-06-14 14:05:56 +0200

    ejercicio02

--- EjerciciosOB-Java/src/ejercicio/Ejercicio02.java
+++ EjerciciosOB-Java/src/ejercicio/Ejercicio02.java
@@ -1,9 +1,23 @@
 package ejercicio;
 
 public class Ejercicio02 {
+	final static double IVA = 0.21;
 
 	public static void main(String[] args) {
-
+		double precioInicial = 10.5;
+		double precioFinal;
+		
+		precioFinal = precioConIVA(precioInicial);
+		
+		System.out.println(precioFinal);
+	}
+	
+	static double precioConIVA(double precioInicial) {
+		double precioFinal;
+		
+		precioFinal = precioInicial + precioInicial*IVA;
+		
+		return precioFinal;
 	}
 
 }
