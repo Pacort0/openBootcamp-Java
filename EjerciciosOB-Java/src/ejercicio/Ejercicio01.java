0f09215200dc0cf9b36eb94748d1c83fe0a57e69
Admin <Admin@DESKTOP-FNNFMRC.home>
2023-06-14 13:57:52 +0200

    primer commit

---
+++ EjerciciosOB-Java/src/ejercicio/Ejercicio01.java
@@ -0,0 +1,20 @@
+package ejercicio;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		int entero = 10;
+		long largo = 12345678912l;
+		double doble = 12.0;
+		boolean booleano = true;
+		String cadena = "Primer ejercicio";
+		
+		System.out.println(entero);
+		System.out.println(largo);
+		System.out.println(doble);
+		System.out.println(booleano);
+		System.out.println(cadena);
+
+	}
+
+}
